a95e4be9b55a94a0217e95775d262659dfe88f93
Jose Aguirre <josjesus17@gmail.com>
2021-08-16 18:25:48 -0500

    Estructura del proyecto StarWars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EpisodioComponent } from './components/episodio/episodio.component';
+import { SaberMasComponent } from './components/saber-mas/saber-mas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    EpisodioComponent,
+    SaberMasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
